5ac734b7e9b09e29ae10204f98a4f18dfc315bcf
Evan Caudy <141912304+EvanCaudy10@users.noreply.github.com>
2025-03-26 22:13:18 -0500

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -3,33 +3,123 @@
 # | should be the only file that you run, anything else should be a module
 # | separate functions into their own modules as needed
 
-from Card import Card
+from Deck import Deck
+from Player import Player
+from Dealer import Dealer
+import os
+import time
 
 def main():
-    # Right now, this driver just demonstrates functionality of our prototype.
-    # in the future, this file will run the game.
+    decklist = Deck()
+    decklist.create_deck()
+    decklist.shuffle_deck()
 
-    queen_of_spades = Card(1, 12)
-    print(queen_of_spades.get_human_value())
-    print(queen_of_spades.get_human_suite())
-    print()
+    p1 = Player()
+    dealer = Dealer()
+    players = [p1, dealer]
 
-    king_of_clubs = Card(2, 13)
-    print(king_of_clubs.get_human_value())
-    print(king_of_clubs.get_human_suite())
-    # This is an error!!! it prints 0 of Clubs...
-    print()
+    for x in range(2):
+        for player in players:
+            player.draw(decklist.draw_card())
 
-    jack_of_diamonds = Card(3, 11)
-    print(jack_of_diamonds.get_human_value())
-    print(jack_of_diamonds.get_human_suite())
-    # another error, should be jack, gives ace?
-    print()
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    ten_of_hearts = Card(4, 10)
-    print(ten_of_hearts.get_human_value())
-    print(ten_of_hearts.get_human_suite())
-    print()
+        p1_value = p1.count_hand()
+        dealer_value = dealer.count_hand()
+
+        if dealer_value != 21:
+            if p1_value != 21 or p1_value == 21 and len(p1.get_hand()) > 2:
+                print('Their Hand:')
+                print(f'[{dealer.get_hand()[0].get_name()}]', end=' ')
+                print(f'[]\n')
+                display_player_cards(p1.get_hand(), p1_value)
+                user_choice = choice()
+
+                if user_choice.upper() == 'H':
+                    p1.draw(decklist.draw_card())
+                    p1_value = p1.count_hand()
+
+                    if p1_value > 21:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+                        print('\nYou busted, you lose!')
+                        break
+
+                else:
+                    while dealer_value < 17:
+                        time.sleep(1.5)
+                        os.system('cls' if os.name == 'nt' else 'clear')
+
+                        dealer.draw(decklist.draw_card())
+                        dealer_value = dealer.count_hand()
+
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    if dealer_value > 21:
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+                        print('\nDealer busts, you win!')
+                        break
+
+                    if p1_value > dealer_value:
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+                        print(f'\n{p1_value} beats {dealer_value}, you win!')
+                    elif p1_value == dealer_value:
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+                        print(f'\n{p1_value} ties {dealer_value}, you push!')
+                    else:
+                        display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                        print()
+                        display_player_cards(p1.get_hand(), p1_value)
+                        print(f'\n{p1_value} loses to {dealer_value}, you lose!')
+
+                    break
+            else:
+                display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+                print()
+                display_player_cards(p1.get_hand(), p1_value)
+                print('\nYou have Black Jack, you win!')
+                break
+        else:
+            display_player_cards(dealer.get_hand(), dealer_value, 'Their')
+            print()
+            display_player_cards(p1.get_hand(), p1_value)
+            print('\nThe dealer has Black Jack, you lose!')
+            break
+
+
+def display_player_cards(hand, player_value, player='Your'):
+    print(f'{player} Hand:')
+    for card in hand:
+        print(f'[{card.get_name()}]', end=' ')
+    print(f'[Value: {player_value}]')
+
+
+def choice():
+    user = input('\nHit or Stand | Type H or S: ')
+    while True:
+        try:
+            if user.upper() == 'H' or user.upper() == 'S':
+                break
+            else:
+                user = input('Not a valid input | Type H or S: ')
+        except:
+            pass
+
+    return user
+
 
 if __name__ == '__main__':
     main()
