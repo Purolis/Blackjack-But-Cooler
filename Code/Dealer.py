13336268e3f26d734f301df52b1e24319eda5f89
Jerry Cai <jerrycai92@gmail.com>
2025-03-29 22:35:26 -0500

    Update Dealer with comments

--- Code/Dealer.py
+++ Code/Dealer.py
@@ -1,15 +1,20 @@
 class Dealer:
+
+    # Default Variables
     __hand_value = None
     __hand = []
 
+    # Initialization
     def __init__(self, hand, hand_value):
         self.set_hand(hand)
         self.set_hand_value(hand_value)
 
+    # Draw a card function
     def draw(self, card):
         self.__hand.append(card)
         return self.__hand
 
+    # Counts the number in the hand function
     def count_hand(self):
         self.__hand_value = 0
         for card in self.__hand:
@@ -22,12 +27,14 @@ class Dealer:
 
         return self.__hand_value
 
+    # Getters
     def get_hand(self):
         return self.__hand
 
     def get_hand_value(self):
         return self.__hand_value
 
+    # Setters
     def set_hand(self, hand):
         self.__hand = hand
 
