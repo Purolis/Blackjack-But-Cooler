5fefe04ceb51db70d7aaa65580382ba5eee06d66
Evan Caudy <141912304+EvanCaudy10@users.noreply.github.com>
2025-04-10 07:19:33 -0500

    Add image attribute to allow for GUI integration

--- Code/Card.py
+++ Code/Card.py
@@ -4,12 +4,14 @@ class Card:
     __value = None
     __suit = None
     __name = None
+    __img = None
 
     # Initialization
-    def __init__(self, value, suit, name):
+    def __init__(self, value, suit, name, img):
         self.set_value(value)
         self.set_suit(suit)
         self.set_card_name(name)
+        self.set_img(img)
 
     # Getters
     def get_value(self):
@@ -21,6 +23,9 @@ class Card:
     def get_card_name(self):
         return self.__name
 
+    def get_img(self):
+        return self.__img
+
     # Setters
     def set_value(self, value):
         self.__value = value
@@ -30,3 +35,6 @@ class Card:
 
     def set_card_name(self, name):
         self.__name = name
+
+    def set_img(self, img):
+        self.__img = img
